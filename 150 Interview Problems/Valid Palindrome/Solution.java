0a5c6c5fc613bced6e84e925ba68894a4cfc6884
DragosMoro <morozan.dragos@gmail.com>
2023-07-09 00:06:04 +0300

    new

---
+++ 150 Interview Problems/Valid Palindrome/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Objects;
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        if(s.length() == 0)
+        {
+            return true;
+        }
+        for(int i=0;i<=s.length()/2;i++)
+        {
+            if(!Objects.equals(s.charAt(i),s.charAt(s.length()-i-1) ))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
