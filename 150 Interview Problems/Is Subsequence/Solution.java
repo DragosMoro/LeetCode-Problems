a17a6f90f4a563fa3b8c7f1d5992e5d9fd05ca15
DragosMoro <morozan.dragos@gmail.com>
2023-07-09 00:18:28 +0300

    new

---
+++ 150 Interview Problems/Is Subsequence/Solution.java
@@ -0,0 +1,27 @@
+import java.util.Objects;
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int pos = 0;
+        if(s.length()>t.length())
+        {
+            return false;
+        }
+        if(s.length() == 0)
+        {
+            return true;
+        }
+        for(int i=0;i<t.length();i++)
+        {
+            if(Objects.equals(t.charAt(i), s.charAt(pos)))
+            {
+                pos++;
+            }
+            if(Objects.equals(pos, s.length()))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
