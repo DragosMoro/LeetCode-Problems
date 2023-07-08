14dabea9ed6ad1b588c3dc3b4374e54c62d2f49c
DragosMoro <morozan.dragos@gmail.com>
2023-07-09 00:23:14 +0300

    Update Solution.java

--- 150 Interview Problems/Is Subsequence/Solution.java
+++ 150 Interview Problems/Is Subsequence/Solution.java
@@ -25,3 +25,26 @@ class Solution {
         return false;
     }
 }
+
+//better time
+// class Solution {
+//     public boolean isSubsequence(String s, String t) {
+//         int pos = 0;
+//         if(s.length() == 0)
+//         {
+//             return true;
+//         }
+//         for(int i=0;i<t.length();i++)
+//         {
+//             if(t.charAt(i) == s.charAt(pos))
+//             {
+//                 pos++;
+//             }
+//             if(pos == s.length())
+//             {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
