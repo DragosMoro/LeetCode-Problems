65d36eb4ded2e2ed3a497f330dc50b7cd07a13be
DragosMoro <morozan.dragos@gmail.com>
2023-08-03 23:57:17 +0300

    Create Solution.java

---
+++ 150 Interview Problems/Group Anagrams/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List> dict = new HashMap<>();
+        if(strs.length<1)
+        {
+            List<List<String>> lista = new ArrayList<>();
+            return lista;
+        }
+        for(int i=0;i<strs.length;i++)
+        {
+
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            if(dict.containsKey(toString(chars)))
+            {
+                
+            }
+            // dict.put(toString(chars), strs[i]);
+            
+
+        }
+        return dict;      
+        }
+}
