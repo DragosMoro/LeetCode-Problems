c5518859dd1e8e2af7b03778981035d9f6363104
DragosMoro <morozan.dragos@gmail.com>
2023-08-11 19:51:11 +0300

    Update Solution.java

--- 150 Interview Problems/Find Peak Element/Solution.java
+++ 150 Interview Problems/Find Peak Element/Solution.java
@@ -1,3 +1,25 @@
-public class Solution {
-    
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
+        int mid = 0;
+        while(left<=right)
+        {
+            mid =left+(right-left)/2;
+            if((mid-1<0 || nums[mid-1] < nums[mid]) && (mid+1==nums.length || nums[mid+1] < nums[mid]))
+            {
+                return mid;
+            } 
+            if(mid>0 && mid<nums.length && nums[mid-1] > nums[mid] && nums[mid]>nums[mid+1])
+            {
+                right = mid-1;
+                            
+            }
+            else{
+              left=mid+1;
+
+            }
+        }
+        return right;
+    }
 }
