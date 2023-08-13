c4a210aebd160508a6bd9739f085f2d573bc2908
DragosMoro <morozan.dragos@gmail.com>
2023-08-13 21:03:15 +0300

    Update Solution.java

--- 150 Interview Problems/Arranging Coins/Solution.java
+++ 150 Interview Problems/Arranging Coins/Solution.java
@@ -1,5 +1,24 @@
-package Arranging Coins;
+class Solution {
+    public int arrangeCoins(int n) {
+        long left = 0;
+        long right = n;
+        while(left<=right)
+        {
+           long mid = left+(right - left)/2;
+           long sum  = mid*(mid+1)/2;
+           if(sum == n)
+           {
+               return (int)mid;
+           }
+           if(n<sum)
+           {
+              right = mid-1;
+           } 
+           else{
+               left = mid+1;
+           }
+        }
+        return (int)right;
 
-public class Solution {
-    
+    }
 }
