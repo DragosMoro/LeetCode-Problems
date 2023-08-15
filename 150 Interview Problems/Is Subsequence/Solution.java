dec6c3adf30b3d192edfc38c6c087a8b43818f89
DragosMoro <morozan.dragos@gmail.com>
2023-08-15 23:34:10 +0300

    Update Solution.java

--- 150 Interview Problems/Is Subsequence/Solution.java
+++ 150 Interview Problems/Is Subsequence/Solution.java
@@ -1,30 +1,30 @@
 import java.util.Objects;
 
-class Solution {
-    public boolean isSubsequence(String s, String t) {
-        int pos = 0;
-        if(s.length()>t.length())
-        {
-            return false;
-        }
-        if(s.length() == 0)
-        {
-            return true;
-        }
-        for(int i=0;i<t.length();i++)
-        {
-            if(Objects.equals(t.charAt(i), s.charAt(pos)))
-            {
-                pos++;
-            }
-            if(Objects.equals(pos, s.length()))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-}
+// class Solution {
+//     public boolean isSubsequence(String s, String t) {
+//         int pos = 0;
+//         if(s.length()>t.length())
+//         {
+//             return false;
+//         }
+//         if(s.length() == 0)
+//         {
+//             return true;
+//         }
+//         for(int i=0;i<t.length();i++)
+//         {
+//             if(Objects.equals(t.charAt(i), s.charAt(pos)))
+//             {
+//                 pos++;
+//             }
+//             if(Objects.equals(pos, s.length()))
+//             {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
 
 //better time
 class Solution {
