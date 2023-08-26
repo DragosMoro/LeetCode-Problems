e96cec2d802e9175847d9bfde44e00abd3cff17f
DragosMoro <morozan.dragos@gmail.com>
2023-08-26 23:59:05 +0300

    add

--- 150 Interview Problems/Lowest Common Ancestor of a Binary Search Tree/Solution.java
+++ 150 Interview Problems/Lowest Common Ancestor of a Binary Search Tree/Solution.java
@@ -1,10 +1,3 @@
-  public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode(int x) { val = x; }
-  }
- 
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
