2942e733bb62deecf85c573026071e5239ca946b
DragosMoro <morozan.dragos@gmail.com>
2023-08-29 23:40:49 +0300

    Create Solution.java

---
+++ 150 Interview Problems/Binary Tree Inorder Traversal/Solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private List<Integer> finalList = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        dfs(root);
+        return finalList;
+    }
+    private void dfs(TreeNode root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        dfs(root.left);
+        finalList.add(root.val);
+        dfs(root.right);
+    }
+}
