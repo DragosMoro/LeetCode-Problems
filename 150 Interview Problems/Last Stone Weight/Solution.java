0585b2aef240d8186bb27516850359f00b68396f
DragosMoro <morozan.dragos@gmail.com>
2023-09-08 21:14:30 +0300

    Create Solution.java

---
+++ 150 Interview Problems/Last Stone Weight/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Collections;
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int el:stones)
+        {
+            pq.offer(el);
+        }
+        while(pq.size()>1)
+        {
+            int first= pq.poll();
+            int second = pq.poll();
+            if(first!=second)
+            {
+                pq.offer(first-second);
+            }
+        }
+        if(pq.size()==1)
+        {
+            return pq.peek();    
+        }
+        return 0;
+        
+    }
+}
