72fa3fcc0fc5bfcd3ec0e3dbd473b6bf66a91047
DragosMoro <morozan.dragos@gmail.com>
2023-09-08 21:15:14 +0300

    Create Solution.java

---
+++ 150 Interview Problems/K Closest Points to Origin/Solution.java
@@ -0,0 +1,24 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> Integer.compare(
+            (b[0]*b[0]+b[1]*b[1]),
+            (a[0]*a[0]+a[1]*a[1])
+        ));
+        for(int[] elems:points)
+        {
+            pq.offer(elems);
+            if(pq.size()>k)
+            {
+                pq.poll();
+            }
+        }
+        int[][] res = new int[k][2];
+        for(int i=0;i<k;i++)
+        {
+            res[i] = pq.poll();
+        }
+        return res;
+    }
+}
