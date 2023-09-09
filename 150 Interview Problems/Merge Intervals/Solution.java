1593badf48c1f86aea79ce64435b1beabda0453f
DragosMoro <morozan.dragos@gmail.com>
2023-09-09 16:29:22 +0300

    Create Solution.java

---
+++ 150 Interview Problems/Merge Intervals/Solution.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if(intervals.length<=1)
+        {
+            return intervals;
+        }
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+        List<int[]> result = new ArrayList<>();
+        int[] prev = new int[2];
+        prev = intervals[0];
+        for(int i=1;i<intervals.length;i++)
+        {
+            if(prev[1]>=intervals[i][0])
+            {
+                prev[1] = Math.max(prev[1], intervals[i][1]);
+            }
+            else{
+                result.add(prev);
+                prev = intervals[i];
+            }
+        }
+        result.add(prev);
+        
+        int [][] ret = new int[result.size()][2];
+        int c = 0;
+        for(int[] res: result)
+        {
+            ret[c] = res;
+            c++;
+        }
+        return ret;
+
+    }
+}
