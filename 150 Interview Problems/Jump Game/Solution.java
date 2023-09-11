f1f73b32cbcb16615da9b868fe6da29d233d9c1d
DragosMoro <morozan.dragos@gmail.com>
2023-09-11 23:39:38 +0300

    Create Solution.java

---
+++ 150 Interview Problems/Jump Game/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int reachable = 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i>reachable)
+            {
+                return false;
+            }
+            reachable = Math.max(nums[i]+i, reachable);
+        }
+        return true;
+    }
+}
