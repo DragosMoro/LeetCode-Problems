038ae93e7fd8965e90ffd95e6946c7bcb20237c1
Dragoș-Emilian Morozan <77979309+DragosMoro@users.noreply.github.com>
2023-09-15 23:44:44 +0300

    Runtime: 21 ms (94.89%), Memory: 46.6 MB (31.72%) - Gitcode

---
+++ 560-subarray-sum-equals-k/560. Subarray Sum Equals K.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> sumOcurrances = new HashMap<>();
+        int sum = 0;
+        int result = 0;
+        //pentru ca acesta ne asigura sa includem si intreaga secventa actuala
+        sumOcurrances.put(0,1); 
+        for(int i=0;i<nums.length;i++)
+        {
+            sum +=nums[i];
+            if(sumOcurrances.containsKey(sum-k))
+            {
+                result +=sumOcurrances.get(sum-k);
+            }
+            if(!sumOcurrances.containsKey(sum))
+            {
+                sumOcurrances.put(sum, 1);
+            }
+            else{
+                sumOcurrances.put(sum, sumOcurrances.get(sum)+1);
+            }
+        }
+        return result;
+    }
+}
