70951da202afcbf287f5ba8371487bb3a30c37e7
Dragoș-Emilian Morozan <77979309+DragosMoro@users.noreply.github.com>
2023-10-06 17:36:27 +0300

    Runtime: 7 ms (38.04%), Memory: 46.4 MB (89.59%) - Gitcode

---
+++ 49-group-anagrams/49. Group Anagrams.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> dict = new HashMap<>();
+        List<List<String>> finalList = new ArrayList<>();
+        if(strs.length<1)
+        {
+            return finalList;
+        }
+        for(int i=0;i<strs.length;i++)
+        {
+
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            String str = new String(chars);
+            if(dict.containsKey(str))
+            {
+                List<String> elem = dict.get(str);
+                elem.add(strs[i]);
+                dict.put(str, elem);
+            }
+            else{
+                 List<String> elem = new ArrayList<>();
+                elem.add(strs[i]);
+                dict.put(str, elem);    
+            }
+                
+            
+        }
+
+        for(Map.Entry<String, List<String>> entry: dict.entrySet())
+        {
+            finalList.add(entry.getValue());
+        }    
+        return finalList;
+        }
+}
