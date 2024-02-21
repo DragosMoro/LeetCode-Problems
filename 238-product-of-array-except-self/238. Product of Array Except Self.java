18b3aa0d20295bf4959a57e0f50fd5f907dad9cf
Dragoș-Emilian Morozan <77979309+DragosMoro@users.noreply.github.com>
2024-02-21 19:03:39 +0200

    Runtime: 1 ms (100.00%), Memory: 51.8 MB (99.56%) - Gitcode

--- 238-product-of-array-except-self/238. Product of Array Except Self.java
+++ 238-product-of-array-except-self/238. Product of Array Except Self.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] res = new int[nums.length];
-        int prod = 1;
+        int product = 1;
         for(int i=0;i<nums.length;i++)
         {
-            res[i] = prod;
-            prod = prod* nums[i];
+            res[i]=product;
+            product = nums[i]*product;
         }
-        prod = 1;
+        product = 1;
         for(int i=nums.length-1;i>=0;i--)
         {
-            res[i] = res[i] *prod;
-            prod = prod* nums[i];
+            res[i] = res[i]*product;
+            product = nums[i]*product;
         }
         return res;
     }
