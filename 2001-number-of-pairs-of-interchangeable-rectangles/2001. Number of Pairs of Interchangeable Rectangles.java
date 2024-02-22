4c3ffdd593450c841186275ab89939a95b820811
Dragoș-Emilian Morozan <77979309+DragosMoro@users.noreply.github.com>
2024-02-22 13:40:55 +0200

    Runtime: 47 ms (53.19%), Memory: 92.2 MB (63.30%) - Gitcode

--- 2001-number-of-pairs-of-interchangeable-rectangles/2001. Number of Pairs of Interchangeable Rectangles.java
+++ 2001-number-of-pairs-of-interchangeable-rectangles/2001. Number of Pairs of Interchangeable Rectangles.java
@@ -1,24 +1,16 @@
 class Solution {
     public long interchangeableRectangles(int[][] rectangles) {
         Map<Double, Long> map = new HashMap<>();
+        long counter = 0;
         for(int i= 0;i<rectangles.length;i++)
         {
-             double key = (double) rectangles[i][0] / rectangles[i][1];
+            double key = (double) rectangles[i][0] / rectangles[i][1];
             if (map.containsKey(key)) {
-                long value = (long) map.get(key)+1;
-                map.put(key, value);
-            } else {
-                map.put(key, 1L);
+                counter += map.get(key);
             }
+            long value = (long) map.getOrDefault(key,0L)+1;
+            map.put(key, value);
             
-        }
-        long counter = 0;
-        for(Map.Entry<Double, Long> entry: map.entrySet())
-        {
-            if(entry.getValue()>=2)
-            {
-                counter += ((entry.getValue()*(entry.getValue()-1))/2);
-            }
         }
         return counter;
     }
