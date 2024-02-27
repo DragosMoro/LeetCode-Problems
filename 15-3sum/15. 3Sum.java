a27145c2a2c338a95a03e8880eec9d35d552b7ea
Dragoș-Emilian Morozan <77979309+DragosMoro@users.noreply.github.com>
2024-02-27 13:23:03 +0200

    Runtime: 27 ms (90.65%), Memory: 52.5 MB (19.54%) - Gitcode

--- 15-3sum/15. 3Sum.java
+++ 15-3sum/15. 3Sum.java
@@ -2,8 +2,7 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-
-        for(int i=0;i<nums.length-1;i++)
+        for(int i=0;i<nums.length-2;i++)
         {
             if(i>0 && nums[i] == nums[i-1])
             {
@@ -13,21 +12,23 @@ class Solution {
             int right = nums.length-1;
             while(left<right)
             {
-                int sum = nums[i]+nums[right]+nums[left];
-                if(sum<0)
-                {
-                    left++;
-                }
-                else if(sum>0)
+                if(nums[i]+nums[left]+nums[right] >0)
                 {
                     right--;
+                }
+                else if(nums[i]+nums[left]+nums[right] <0)
+                {
+                    left++;
                 }
                 else{
-                    List<Integer> triple = Arrays.asList(nums[i], nums[right], nums[left]);
-                    res.add(triple);
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[left]);
+                    list.add(nums[right]);
+                    res.add(list);
                     left++;
                     right--;
-                    while(left<right && nums[left] ==nums[left-1])
+                    while(left<right && nums[left] == nums[left-1])
                     {
                         left++;
                     }
