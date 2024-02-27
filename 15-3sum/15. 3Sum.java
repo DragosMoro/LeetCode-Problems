869608e576e9bedf77c03427771e57bbcae8a05f
Dragoș-Emilian Morozan <77979309+DragosMoro@users.noreply.github.com>
2024-02-27 13:18:14 +0200

    Runtime: 27 ms (90.65%), Memory: 50 MB (99.85%) - Gitcode

--- 15-3sum/15. 3Sum.java
+++ 15-3sum/15. 3Sum.java
@@ -1,36 +1,42 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> set = new HashSet<>();
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0;i<nums.length-2;i++)
+
+        for(int i=0;i<nums.length-1;i++)
         {
+            if(i>0 && nums[i] == nums[i-1])
+            {
+                continue;
+            }
             int left = i+1;
             int right = nums.length-1;
             while(left<right)
             {
-                if(nums[i]+nums[left]+nums[right] >0)
-                {
-                    right--;
-                }
-                else if(nums[i]+nums[left]+nums[right] <0)
+                int sum = nums[i]+nums[right]+nums[left];
+                if(sum<0)
                 {
                     left++;
+                }
+                else if(sum>0)
+                {
+                    right--;
                 }
                 else{
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[left]);
-                    list.add(nums[right]);
-                    set.add(list);
+                    List<Integer> triple = Arrays.asList(nums[i], nums[right], nums[left]);
+                    res.add(triple);
                     left++;
                     right--;
+                    while(left<right && nums[left] ==nums[left-1])
+                    {
+                        left++;
+                    }
+                    while(left<right && nums[right] == nums[right+1])
+                    {
+                        right--;
+                    }
                 }
             }
-        }
-        for(List<Integer>lista:set)
-        {
-            res.add(lista);
         }
         return res;
     }
