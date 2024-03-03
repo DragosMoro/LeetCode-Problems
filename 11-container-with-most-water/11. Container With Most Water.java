b1722b4b7d7ef1b886f86883fde555f91759dc26
Dragoș-Emilian Morozan <77979309+DragosMoro@users.noreply.github.com>
2024-03-03 23:36:24 +0200

    Runtime: 6 ms (11.48%), Memory: 57.7 MB (63.70%) - Gitcode

--- 11-container-with-most-water/11. Container With Most Water.java
+++ 11-container-with-most-water/11. Container With Most Water.java
@@ -1,33 +1,24 @@
 class Solution {
     public int maxArea(int[] height) {
-       int left=0;
-       int right = height.length-1;
-       int max=0;
-       while(left<right)
-       {
-           int verif = Math.min(height[left],height[right]) * (right-left);
-           if( verif > max)
-           {
-               max = verif;
-           }
-           if(height[left]<height[right])
-           {
-               left++;
-           }
-           else if(height[left]>height[right])
-           {
-               right--;
-           }
-           else{
-               if(height[left+1]<height[right-1])
-               {
-                   right--;
-               }
-               else{
-                   left++;
-               }
-           }
-       }
-       return max;
+        int max = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int val = Math.min(height[left], height[right]) * (right - left);
+            max = Math.max(val, max);
+            if (height[left] < height[right]) {
+                left++;
+            } else if (height[left] > height[right]) {
+                right--;
+            } else {
+                if (height[left + 1] < height[right - 1]) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return max;
     }
+
 }
