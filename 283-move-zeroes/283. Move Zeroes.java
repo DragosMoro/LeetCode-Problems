148f522bd5b9cbd66722620e0999b3f55cfe16bc
Dragoș-Emilian Morozan <77979309+DragosMoro@users.noreply.github.com>
2024-03-17 19:40:24 +0200

    Runtime: 2 ms (84.68%), Memory: 46.2 MB (37.83%) - Gitcode

--- 283-move-zeroes/283. Move Zeroes.java
+++ 283-move-zeroes/283. Move Zeroes.java
@@ -1,18 +1,12 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int nonZeroIndex = 0; // Indică unde ar trebui să plasăm următorul element non-zero.
+        int nonZeroIndex = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                // În cazul în care `i` este diferit de `nonZeroIndex`, înseamnă că am întâlnit
-                // zerouri înainte,
-                // și putem face un swap între elementul curent non-zero și primul zero
-                // întâlnit.
                 if (i != nonZeroIndex) {
                     nums[nonZeroIndex] = nums[i];
                     nums[i] = 0;
                 }
-                // Avansăm `nonZeroIndex` pentru a indica următoarea poziție pentru un element
-                // non-zero.
                 nonZeroIndex++;
             }
         }
