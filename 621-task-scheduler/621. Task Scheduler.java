12a7cea86bfa51508e6e6fae90f42f3ed05f9f06
Dragoș-Emilian Morozan <77979309+DragosMoro@users.noreply.github.com>
2024-03-19 16:02:21 +0200

    Runtime: 25 ms (43.79%), Memory: 45.8 MB (37.32%) - Gitcode

--- 621-task-scheduler/621. Task Scheduler.java
+++ 621-task-scheduler/621. Task Scheduler.java
@@ -1,20 +1,38 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        // frequencies of the tasks
-        int[] frequencies = new int[26];
-        for (int t : tasks) {
-            frequencies[t - 'A']++;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+        int[] freqChar = new int[26];
+        for(int i=0;i<tasks.length;i++)
+        {
+            freqChar[tasks[i]-'A']++;
+        }
+        for(int i=0;i<freqChar.length;i++)
+        {
+            if(freqChar[i]>0)
+            {
+                maxHeap.add(freqChar[i]);
+            }
+        }
+        int counter = 0;
+        while (!maxHeap.isEmpty() || !q.isEmpty()) 
+        {
+            if (!maxHeap.isEmpty()) {
+                int top = maxHeap.poll() - 1;
+                if (top > 0) {
+                    q.add(new Pair<>(top, counter + n));
+                }
+            }
+    
+            while (!q.isEmpty() && q.peek().getValue() <= counter) {
+                maxHeap.add(q.poll().getKey());
+            }
+            
+            counter++;
         }
 
-        Arrays.sort(frequencies);
+        return counter;
 
-        int fMax = frequencies[25]-1;
-        int idleTime = fMax * n;
-        
-       for(int i=24;i>=0;i--)
-       {
-           idleTime -= Math.min(fMax, frequencies[i]);
-       }
-       return idleTime>0 ? idleTime+tasks.length : tasks.length;
+
     }
 }
