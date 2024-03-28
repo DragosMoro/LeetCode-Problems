5b66a591d84a41b3d54cc27c286041c2c5dda731
Dragoș-Emilian Morozan <77979309+DragosMoro@users.noreply.github.com>
2024-03-28 11:07:19 +0200

    Runtime: 4 ms (64.24%), Memory: 44.7 MB (58.76%) - Gitcode

--- 1-two-sum/1. Two Sum.java
+++ 1-two-sum/1. Two Sum.java
@@ -1,23 +1,22 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for(int i=0;i<nums.length;i++)
-        {
-            map.put(nums[i], i);
+        int requestedNumber = 0;
+        int result[] = new int[2];
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        for (int i=0;i<nums.length;i++){
+            hashMap.put(nums[i],i);
         }
-
-        int [] res = new int[2];
-        for(int i=0;i<nums.length;i++)
-        {
-            int searched = target-nums[i];
-            if(map.containsKey(searched) && map.get(searched)!=i)
-            {
-                res[0] = i;
-                res[1] = map.get(searched);
-                return res;
+        for (int i=0; i< nums.length;i++){
+            requestedNumber = target - nums[i];
+            if(hashMap.containsKey(requestedNumber) && hashMap.get(requestedNumber)!=i){
+                result[0] = i;
+                result[1] = hashMap.get(requestedNumber);
+                break;
             }
+        
+
+        
         }
-        return res;
-    }
+    return result;
+    }   
 }
