093ad95a28a855dc8b929a26a165a13a4cd51a32
Dragoș-Emilian Morozan <77979309+DragosMoro@users.noreply.github.com>
2024-03-28 22:26:36 +0200

    Runtime: 2 ms (78.26%), Memory: 61.9 MB (28.98%) - Gitcode

--- 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.java
+++ 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.java
@@ -1,26 +1,16 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        if(prices.length<2)
-        {
-            return 0;
-        }
+  public int maxProfit(int[] prices) {
         int left = 0;
         int right = 1;
-        int max = 0;
-        int current = 0;
-        while(right<prices.length)
-        {
-            current = prices[right] - prices[left];
-            if(max<current)
-            {
-                max = current;
-            }
-            while(prices[left]>prices[right] && left<right)
-            {
-                left++;
+        int maxProfit = 0;
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
+            } else {
+                left = right;
             }
             right++;
         }
-        return max;
+        return maxProfit;
     }
 }
