08ace0d14209f802ffcf9f82d2ee62230e28e364
Dragoș-Emilian Morozan <77979309+DragosMoro@users.noreply.github.com>
2024-04-11 17:42:41 +0300

    Runtime: 77 ms (19.49%), Memory: 45.4 MB (67.79%) - Gitcode

--- 402-remove-k-digits/402. Remove K Digits.java
+++ 402-remove-k-digits/402. Remove K Digits.java
@@ -2,10 +2,10 @@ import java.util.Stack;
 
 class Solution {
     public String removeKdigits(String num, int k) {
-        Stack<Integer> stack = new Stack<>();
-        for(int i=0;i<num.length();i++) {
-            int digit = Character.getNumericValue(num.charAt(i));
-            while(!stack.isEmpty() && k > 0 && stack.peek() > digit) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char digit : num.toCharArray()) {
+            while (!stack.isEmpty() && k > 0 && stack.peek() > digit) {
                 stack.pop();
                 k--;
             }
